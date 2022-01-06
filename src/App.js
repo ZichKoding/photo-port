1ed54194e45e28440a9293344882ac67521b7ef8
Chris Zichko <chriszichkocoding@gmail.com>
2022-01-06 15:44:27 -0500

    ..

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import About from './components/About/About';
 import Nav from './components/Nav/Nav';
+import Gallery from './components/Gallery/Gallery';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
